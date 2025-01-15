4a06a6e373829785494eeb677050a75361443f40
Muralidhar Maddali <143109142+muralidharmaddali@users.noreply.github.com>
2025-01-15 20:06:57 +0530

    Create Entry Check.cpp

---
+++ Starters 169/Entry Check.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int ag;
+	cin>>ag;
+	if(ag>=10)
+	{
+	    cout<<"YES";
+	}
+	else
+	{
+	    cout<<"NO";
+	}
+	return 0;
+
+}
