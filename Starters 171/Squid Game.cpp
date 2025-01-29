568f2f5a469fdd51fe7a25862c7953899b223719
Muralidhar Maddali <143109142+muralidharmaddali@users.noreply.github.com>
2025-01-29 21:07:14 +0530

    Create Squid Game.cpp

---
+++ Starters 171/Squid Game.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int l1,l2;
+    cin>>l1>>l2;
+    int l3 = l1-l2;
+    cout<<l3*10000<<endl;
+    return 0;
+}
